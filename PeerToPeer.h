6b57eab8960de90b6e6089f4b9004f9d8df7a81e
brdane <67879478+brdane@users.noreply.github.com>
2021-12-23 18:15:24 -0600

    Added more comments.

--- PeerToPeer.h
+++ PeerToPeer.h
@@ -19,7 +19,7 @@ Instagram: @spankedcheese
 
 
 
-//I probably don't need all of this, but better to be safe than sorry.
+//I probably don't need all of these libraries, but better to be safe than sorry.
 #include <string>
 #include <fstream>
 
@@ -40,22 +40,28 @@ Instagram: @spankedcheese
 
 using namespace std;
 
+//A simple structure for our client information.
 struct client
 {
     int host;
     short port;
 };
 
+
 struct client server;
 
-bool bConnected;
+bool bConnected; //Used by Connect(), I can call connect(), but not this.
 
-WSADATA wsa;
-struct sockaddr_in si_me, si_other;
-int s, slen;
+WSADATA wsa; //Basic structure that contains socket information.
+struct sockaddr_in si_me, si_other; //Pointers to both your ip address and the last ip you received data from.
 
-#define BUFLEN 4096
-char buf[BUFLEN];
+int s; //Our socket.
+
+int slen; //Length of si_other.
+
+#define BUFLEN 4096 //The size of each message that we receive. Feel free to change depending on your application.
+
+char buf[BUFLEN]; //A char array that our received message/data goes into.
 
 //Let's you know if you are connected to a socket or not.
 bool Connected()
@@ -84,15 +90,18 @@ sockaddr_in ServerNetwork()
 //Connects to a port of your choosing to create the hole-punch.
 void Connect(unsigned short ip_port)
 {
+	//If we are already connected, then don't try to connect again.
 	if (bConnected)
 		return;
 
+	
 	if (!bConnected)
 	{
 		if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
 		{
 		}
-
+		
+		//Create our socket.
 		if ( (s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
 		{
 		}
@@ -103,13 +112,17 @@ void Connect(unsigned short ip_port)
 			si_me.sin_port = htons(ip_port); // This is not really necessary, we can also use 0 (any ip_port)
 			si_me.sin_addr.s_addr = htonl(INADDR_ANY);
 
+			//If we can't bind and activate our socket, error out and exit.
 			if( bind(s ,(struct sockaddr *)&si_me , sizeof(si_me)) == SOCKET_ERROR)
 			{
 				printf("Bind failed with error code : %d" , WSAGetLastError());
 				return;
 			}
-
+			
+			//Obtain the size of the pointer to the other ip.
 			slen = sizeof(si_other);
+			
+			//Set out connected bool to true.
 			bConnected = true;
 		}
 	}
@@ -119,11 +132,15 @@ void Connect(unsigned short ip_port)
 bool SendToIp(char* server_ip, const char* message)
 {
 
+	//If we don't have an active connection, don't go any further.
 	if (!bConnected)
 		return false;
 
+	//Convert the server_ip parameter.
 	server_ip = (char *)(LPCTSTR)server_ip;
 
+	//Setup our si_other pointer and put in our server_ip
+	//parameter.
 	memset((char *) &si_other, 0, sizeof(si_other));
 	si_other.sin_family = AF_INET;
 	si_other.sin_port = si_me.sin_port;
@@ -131,6 +148,7 @@ bool SendToIp(char* server_ip, const char* message)
 	server.host = si_other.sin_addr.s_addr;
 	server.port = si_other.sin_port;
 
+	//If the message doesn't go through, return false.
 	if (sendto(s, message, strlen(message), 0, (struct sockaddr*)(&si_other), slen) == -1)
 	{
 		return false;
@@ -144,14 +162,19 @@ bool SendToIp(char* server_ip, const char* message)
 //Returns true if you receive data in your connected socket, also returns the data that you received.
 bool received(char* &msg)
 {
-	if (recvfrom(GetSocket(), buf, 4096, 0, (struct sockaddr *) &si_other, &slen) != -1)
+	//Check to see if received something on our socket, if so, point our buf variable to it
+	//and set fill it with BUFLEN. Also, set si_other with information on who sent the data
+	//we received.
+	if (recvfrom(GetSocket(), buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) != -1)
 	{
-
+		//Fetch the data we received to the msg parameter.
 		msg = buf;
-		return true;
+		
+		//return true to let the programmer know that something was received.
+		return true; 
 	}
 else
-	{
+	{	//Otherwise, return false.
 		return false;
 	}
 }
