c22af89980a7ee089b24b4af7b412e1253373b1f
brdane <67879478+brdane@users.noreply.github.com>
2022-01-07 06:27:38 -0600

    Minor bug fixes
    -Now featuring a Command Prompt-friendly version of this library, PeerToPeer_Win32.h, that uses 
    CStdString as a substitute for CString, which is used in MFC Applications. I did that because
    I have had trouble trying to load CStrings into console applications. This version of the library
    will contain the exact updates as the regular version.
    
    -Added EpochSeconds(), returns the current time in Epoch format, as a CString.
    
    -Added Basic file-handling functions that return true or false if they successfully processed.
    
    -File-handling functions are loadfile(), writefile(), appendfile() and foundinfile().
    
    -Added an example MFC program that does Peer-to-Peer chat, used port 6969 as an example.
    Type in a username, and IP to send a message to and the message itself.
    When you receive a message, you don't need to type in an IP, the program will send your message
    to the last IP you received a message to.

--- PeerToPeer_Win32.h
+++ PeerToPeer_Win32.h
@@ -1,19 +1,14 @@
 /*
-Absolute Simple Peer-To-Peer(ASPTP) v1.0
+Absolute Simple Peer-To-Peer (ASPTP) library is an incredibly simple library for C++ programming, 
+to exchange data between computers, with no main-server or middleman required, via peer-to-peer protocol.
+The method used with this library is through UDP hole-punching, which basically temporarily makes an exception 
+in your network's firewall and opens a specified port and socket, in-order to communicate directly to another system.
 
-Brenden Dane, 12/23/2021
+I wrote this library as a frustrated programmer who couldn't find any already made, super bare-bones simple library for
+P2P connections and communication. So, I made one myself and kept it as SUPER simple as I could. I've commented everything 
+I possibly could and explained everything in the simplest form to where a beginner programmer can understand.
 
-
-I wrote this library as a frustrated programmer who couldn't find
-any already made, super bare-bones simple library for P2P connections
-and communication. So, I made one myself and kept it as SUPER simple
-as I could. I've commented everything I possibly could and explained
-everything in the simplest for to where a beginner programmer can
-understand. If you have any questions whatsoever, feel free to contact
-me.
-
-Command Prompt version.
-
+If you have any questions whatsoever, feel free to contact me.
 
 Email: brdane@gmail.com Instagram: @spankedcheese
 
